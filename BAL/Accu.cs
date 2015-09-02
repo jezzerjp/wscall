ae8c0686afaaef0a53aac32a05041cfe1d0bfa96
Jeremy Partridge <jpartridge@teachersassurance.co.uk>
2015-09-02 11:42:08 +0100

    initial version

---
+++ BAL/Accu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL
+{
+    public class Accu : IWeatherSource
+    {
+
+        public double GetTemp()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetWind()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
